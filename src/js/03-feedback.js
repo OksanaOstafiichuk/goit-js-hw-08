78a25e6a19080650a824ac1a0913ed646153e030
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-05-17 01:06:41 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    // email: document.querySelector('input'),
+    // message: document.querySelector('textarea')
+}
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+refs.form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+dataVarification();
+
+function onInputForm(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function dataVarification() {
+    const savedFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (savedFormData) {
+        refs.form.email.value = savedFormData.email;
+        refs.form.message.value = savedFormData.message;
+    }
+
+}
+
+function onSubmitForm(evt) {
+    evt.preventDefault();
+   
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)))
+    evt.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
