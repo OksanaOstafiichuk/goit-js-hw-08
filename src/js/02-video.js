b7fea4c0d177ffeaf8d9cf578d7db9bc59b61f1e
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-05-16 23:47:22 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const savedPlaybackTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+function getPlaybackTime(data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(getPlaybackTime, 1000));
+
+player.setCurrentTime(savedPlaybackTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+
+            default:
+                break;
+    }
+});
